e33b1e4737469092f40d1de15789cda49974f653
nicot290a <34028647+nicot290a@users.noreply.github.com>
2020-06-26 12:43:29 -0300

    Add files via upload
    prueba

---
+++ ComprobacionLogin.php
@@ -0,0 +1,7 @@
+<br />
+<b>Notice</b>:  Undefined index: usuario in <b>/home/c1590108/public_html/sandbox/paradigmas/php/ComprobacionLogin.php</b> on line <b>4</b><br />
+<br />
+<b>Notice</b>:  Undefined index: contrasena in <b>/home/c1590108/public_html/sandbox/paradigmas/php/ComprobacionLogin.php</b> on line <b>5</b><br />
+<br />
+<b>Warning</b>:  Cannot modify header information - headers already sent by (output started at /home/c1590108/public_html/sandbox/paradigmas/php/ComprobacionLogin.php:4) in <b>/home/c1590108/public_html/sandbox/paradigmas/php/ComprobacionLogin.php</b> on line <b>23</b><br />
+{"estado":0,"mensaje":"Usuario y contrase\u00f1a no ingresados"}
